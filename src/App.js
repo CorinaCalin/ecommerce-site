64f79dcc0b27be16f4c70c0894e7c300afbc8a36
Corina <calin_corina64@yahoo.com>
2023-03-25 15:41:03 +0200

    home and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Home } from './components/Home'
 
 export class App extends Component {
   render() {
     return (
-      <div>
-hello world
-      </div>
+<BrowserRouter>
+
+<Switch>
+      <Route exact path='/' component={Home} />
+</Switch>
+      </BrowserRouter>
     )
   }
 }
---
+++ src/config/Config.js
@@ -0,0 +1,33 @@
+import * as firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBmIIlR26n-gmx7qypkqucCmL3OwQSMmzU",
+  authDomain: "ecommerce-site-87874.firebaseapp.com",
+  projectId: "ecommerce-site-87874",
+  storageBucket: "ecommerce-site-87874.appspot.com",
+  messagingSenderId: "832652100092",
+  appId: "1:832652100092:web:a7c4698f94c6c1ea6d3279",
+  measurementId: "G-ZS0CXGH1CV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+firebase.initializeApp(firebase.auth);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+export {auth, db, storage}
