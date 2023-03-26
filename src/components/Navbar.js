973eb53adfdc044d7d2e488a4bbc0fdc047360a9
Corina <calin_corina64@yahoo.com>
2023-03-26 12:25:27 +0300

    Home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../images/ecommerce.svg'
+import logo from '../images/image1.svg'
 
 export const Navbar = () => {
     return (
         <div className='navbox'>
             <div className='leftside'>
-            <img src={logo} alt=""/>
-            <div className='rightside'></div>
+            <img src={logo} alt="" />
+            </div>
+            <div className='rightside'>
             <Link to='/singup' className='navlinks'>SING UP</Link>
             <Link to='/login'className='navlinks'>Login</Link>
         </div>
-        </div>
+    </div>
     )
 }
