0f6e8161ee0719046f67680154ae5599913d9147
Corina <calin_corina64@yahoo.com>
2023-03-27 19:32:24 +0300

    error upload image

--- src/components/AddProducts.js
+++ src/components/AddProducts.js
@@ -1,26 +1,58 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export const AddProducts = () => {
+
+    const [productName, setProductName] = useState('');
+    const [productPrice, setProductPrice] = useState(0);
+    const [productImg, setProductImg] = useState(null);
+    const [error, setError] = useState('');
+
+    const types = ['image/png', 'image/jpeg']
+
+    const productImgHandler = (e) =>{
+        let selectedFile = e.target.files[0];
+        if(selectedFile && types.includes(selectedFile.type))
+            {
+                setProductImg(selectedFile);
+                setError('');
+            }
+                else
+            {
+                setProductImg(null);
+                setError('Please select a valid image type png or jpeg');
+            }
+    }
+
+    const addProduct = (e) =>{
+        e.preventDefault();
+        console.log(productName, productPrice, productImg);
+    }
+
     return (
         <div className='container'>
             <br />
             <h2>ADD PRODUCTS</h2>
             <hr />
-            <form autoComplete="off" className='form-group'>
+            <form autoComplete="off" className='form-group' onSubmit={AddProducts}>
                 <label htmlFor="product-name">Product Name</label>
                 <br />
-                <input type="text" className='form-control' required />
-
+                <input type="text" className='form-control' required 
+                onChange={(e)=>setProductName(e.target.value)} value={productName} />
+                
+                <br />
                 <label htmlFor="product-price">Product price</label>
                 <br />
-                <input type="number" className='form-control' required />
+                <input type="number" className='form-control' required 
+                onChange={(e)=>setProductPrice(e.target.value)} value={productPrice} />
+
                 <br />
                 <label htmlFor="product-img">Product Image</label>
                 <br />
-                <input type="file" className='form-control' />
+                <input type="file" className='form-control' onChange={productImgHandler} />
                 <br />
-                <buttom className='btn btn-success btn-md'>ADD</buttom>  
+                <buttom className='btn btn-success btn-md mybtn'>ADD</buttom>  
             </form>
+                {error && <span>{error}</span>}
         </div>
     )
 }
