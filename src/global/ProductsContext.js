f5cf3dafb162b64a266ef8909b6b621fde30ff73
Corina <calin_corina64@yahoo.com>
2023-04-02 10:47:54 +0300

    show products part2

--- src/global/ProductsContext.js
+++ src/global/ProductsContext.js
@@ -36,9 +36,26 @@ export class ProductsContextProvider extends React.Component{
 
    render(){
     return(
-        <ProductsContext.Provider value ={{products:[...this.state.products]}}>
-            {this.props.children}
-        </ProductsContext.Provider>
+        <>
+            {products.length !== 0 && <h1>Products</h1>}
+            <div className='products-container'>
+                {products.length === 0 && <div>slow internet...no products to display</div>}
+                {products.map(product => (
+                    <div className='product-card' key={product.ProductID}>
+                        <div className='product-img'>
+                            <img src={product.ProductImg} alt="not found" />
+                        </div>
+                        <div className='product-name'>
+                            {product.ProductName}
+                        </div>
+                        <div className='product-price'>
+                            Rs {product.ProductPrice}.00
+                    </div>
+                        <button className='addcart-btn' onClick={() => dispatch({ type: 'ADD_TO_CART', id: product.ProductID, product })}>ADD TO CART</button>
+                    </div>
+                ))}
+            </div>
+        </>
     )
    }
 }
